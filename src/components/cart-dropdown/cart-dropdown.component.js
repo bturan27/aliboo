6bbcade5e46458b8316c89c5f74309ef6032c4a1
bturan27 <begumturann@outlook.com>
2022-07-22 17:58:10 -0700

    alibooo

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,11 +1,13 @@
 import Button from '../button/button.component'
 import './cart-dropdown.styles.scss'
 import React from 'react';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'></div>
+            {[].map(item => <CartItem cartItem= {item} />)};
             <Button>Check Out </Button>
         </div>
     )
