5cbe896c827bb386744e9e2af12fa42289fb09a0
bturan27 <begumturann@outlook.com>
2022-08-01 13:06:07 -0700

    add to aliboo

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -22,7 +22,7 @@ const CheckoutItem = ({cartItem}) => {
             <span className="quantity">
                 <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
                 <span className='value'>{quantity}</span>
-                <div className='arrow' onClick= {addItemHandler}>&#100095;</div>
+                <div className='arrow' onClick= {addItemHandler}>&#10095;</div>
             </span>
             <span className="price">{price}</span>
             <div className="remove-button" onClick={clearItemHandler}> &#10005;</div>
