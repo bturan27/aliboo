6833e82d14156e737ed707c23d2160b6b8c0f6be
bturan27 <begumturann@outlook.com>
2022-08-30 17:39:29 -0700

    styling

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -4,11 +4,14 @@ import CategoriesPreview from '../categories-preview/categories-preview.componen
 
 import './shop.styles.scss';
 
+import Category from '../category/category.component'
+
 const Shop = () => {
 
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path =':category' element={<Category />} />
     </Routes>
   );
 };
