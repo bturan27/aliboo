dcd147e0ffac6f5d3b2c8bc84904897b7fc22618
bturan27 <begumturann@outlook.com>
2022-11-01 22:47:48 -0600

    redux

--- src/routes/category/category.component.js
+++ src/routes/category/category.component.js
@@ -1,32 +1,33 @@
-import  {useContext, useState, useEffect, Fragment} from 'react'
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { CategoriesContext } from '../../contexts/categories.context';
-
-import './category.styles.scss'
 import ProductCard from '../../components/product-card/product-card.component';
-import {useParams} from 'react-router-dom';
+
+import { selectCategoriesMap } from '../../store/categories/categories.selector';
+
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
-    const {category} = useParams();
-    const {categoriesMap} = useContext(CategoriesContext);
+    const { category } = useParams();
+    const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
-    
-    useEffect(() =>{
+
+    useEffect(() => {
         setProducts(categoriesMap[category]);
-    }, [category, categoriesMap])
+    }, [category, categoriesMap]);
 
     return (
-        <Fragment> 
-        <h2 className='category-title'> {category.toUpperCase()} </h2>
-        <div className='category-container'>
-            {products &&
-            products.map((product) => (
-            <ProductCard key={product.id}  product={product} />
-            ))}
-        </div>
+        <Fragment>
+            <Title>{category.toUpperCase()}</Title>
+            <CategoryContainer>
+                {products &&
+                    products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+            </CategoryContainer>
         </Fragment>
-    )
-}
-
+    );
+};
 
 export default Category;
--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,17 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { useEffect } from 'react';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-
+import { useDispatch } from 'react-redux';
 import './shop.styles.scss';
 
 import Category from '../category/category.component'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-
+import { setCategoriesMap } from '../../store/categories/categories.action';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(()=> {
+    const getCategoriesMap = async() =>{
+      const categoryMap = await getCategoriesAndDocuments('collections');
+      dispatch (setCategoriesMap(categoryMap));
+    }
+    getCategoriesMap();
+  }, [])
+
 
   return (
-
       <Routes>
         <Route index element={<CategoriesPreview />} />
         <Route path =':category' element={<Category />} />
